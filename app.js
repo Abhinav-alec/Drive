5b6e56b9dc7dbc61b57da23f51a256906a430f3b
Abhinav kumar jha <abhinav234765@gmail.com>
2025-03-25 02:30:23 +0530

    frontend completed

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ dotenv.config();
 const connectDB = require("./config/db");
 connectDB();
 const cookieParser = require("cookie-parser");
+const indexRouter = require("./routes/index.routes");
 app.set("view engine", "ejs");
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/", indexRouter);
 
 app.use("/user", userRouter);
 
